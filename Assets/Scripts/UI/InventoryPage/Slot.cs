0af259484353476b1dbee702dc59581b92a492b2
ThyRedRock <kolicannon@gmail.com>
2026-04-27 10:00:27 -0400

    Revert "may have other stuff idk"
    This reverts commit 7ba3643a487afd4129b23a4d5c2a46f7f16a2140.

--- Assets/Scripts/UI/InventoryPage/Slot.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
-
-public class Slot : MonoBehaviour
-{
-    public GameObject currenItem; // the item currently held in this slot
-}
