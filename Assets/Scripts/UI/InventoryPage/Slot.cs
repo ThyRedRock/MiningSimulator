e8ad5184f78c617ce8c9b338a89f162fb336c646
ThyRedRock <kolicannon@gmail.com>
2026-04-29 10:01:08 -0400

    inventory stuff

---
+++ Assets/Scripts/UI/InventoryPage/Slot.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject currenItem; // the item currently held in this slot
+}
---
+++ Assets/Scripts/UI/ItemDragHandler.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform originalParent;
+    CanvasGroup canvasGroup;
+   
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }   
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent; // save og parent
+        transform.SetParent(transform.root); //Above other canvas
+        canvasGroup.blocksRaycasts = false; 
+        canvasGroup.alpha = 0.6f; // semi- transparent during drag
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position; // follow the mouse 
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true; //enable raycasts
+        canvasGroup.alpha = 1f; // no longer transparent
+
+        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>(); // slot where item dropped 
+        Slot originalSlot = originalParent.GetComponent<Slot>();
+
+        if(dropSlot != null)
+        {
+            if (dropSlot.currentItem != null)
+            {
+                //Is a slot under drop point
+                dropSlot.currentItem.transform.SetParent(originalSlot.transform);
+                originalSlot.currentItem = dropSlot.currentItem;
+                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            }
+            else
+            {
+                originalSlot.currentItem = null;
+            }
+
+            //move item into drop slot
+            transform.SetParent(dropSlot.transform);
+            dropSlot.currentItem = gameObject;
+        }
+        else
+        {
+            //no slot under drop point 
+            transform.SetParent(originalParent);
+        }
+
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero; //center
+
+
+    }
+
+}
