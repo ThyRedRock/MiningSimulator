79e7005d6196b50fce86202f86e703994fa28cbb
ThyRedRock <kolicannon@gmail.com>
2026-04-29 08:38:38 -0400

    inventory stuff

---
+++ Assets/Scripts/UI/InventoryPage/ItemDragHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    void Start()
+    {
+
+    }
+
+
+   public void OnBeginDrag(PointerEventData eventData)
+   {
+    throw new System.NotImplementedException();
+   }
+   
+      public void OnDrag(PointerEventData eventData)
+   {
+    throw new System.NotImplementedException();
+   }
+
+      public void OnEndDrag(PointerEventData eventData)
+   {
+    throw new System.NotImplementedException();
+   }
+}
+
+
