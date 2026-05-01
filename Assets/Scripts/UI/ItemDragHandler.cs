bfa4786bbd92731f344db7f0e5e5c9adba0c41a5
ThyRedRock <kolicannon@gmail.com>
2026-05-01 08:54:38 -0400

    Finished Inventory
    A working inventory system

--- Assets/Scripts/UI/ItemDragHandler.cs
+++ Assets/Scripts/UI/ItemDragHandler.cs
@@ -32,6 +32,14 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         canvasGroup.alpha = 1f; // no longer transparent
 
         Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>(); // slot where item dropped 
+        if(dropSlot == null)
+        {
+            GameObject dropItem = eventData.pointerEnter;
+            if(dropItem != null)
+            {
+                dropSlot = dropItem.GetComponentInParent<Slot>();
+            }
+        }
         Slot originalSlot = originalParent.GetComponent<Slot>();
 
         if(dropSlot != null)
