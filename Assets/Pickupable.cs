f2c66fa0b9751281b32307a6f26beaa80c34cab5
TreshawnB <itstre8r@gmail.com>
2026-05-15 11:26:46 -0400

    Item pick up works and multiple items spawn

--- Assets/Pickupable.cs
+++ Assets/Pickupable.cs
@@ -6,6 +6,11 @@ public class Pickupable : MonoBehaviour
     public bool CanPickup;
     public GameObject Intsystem;
 
+    float slightpickupdelay = 0.7f;
+    float time;
+
+    public GameObject invmanager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,27 +20,35 @@ public class Pickupable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
+        time = time + Time.deltaTime;
         //find the "Player" gameobject so we can call the script (had to do it this way because if we didnt it would get the prefabs script and not the actual player)
         Intsystem = GameObject.Find("playerV2(Clone)");
         //Defines playerInRange, makes the inRange in player int system the same as in this script
         //Makes it where we dont need to define each interactable object in the player script
         playerInRange = Intsystem.GetComponent<PlayerPickupSystem>().inRange;
 
-
-        //if player is close enough to see the interact button
-        if (playerInRange == true && CanPickup == true)
-		{
-            Debug.Log("item in rnage");
-        }	
-        else
+        if (time >= slightpickupdelay)
         {
-            
+            //if player is close enough to see the interact button
+            if (playerInRange == true && CanPickup == true)
+            {
+                Destroy(gameObject);
+                Debug.Log("Pick up +1");
+            }	
+            else
+            {
+                
+            }
         }
     }
     //check to see if the player is in range of this gameobject (this is so we can have multiple interact buttons)
-    void OnTriggerStay2D()
+    void OnTriggerStay2D(Collider2D Other)
     {
-        CanPickup = true;
+        GameObject triggeringObject = Other.gameObject;
+        if (triggeringObject.CompareTag("Player"))
+        {
+            CanPickup = true;
+        }
     }
     void OnTriggerExit2D()
     {
--- Assets/Scripts/MinableScript.cs
+++ Assets/Scripts/MinableScript.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class MinableScript : MonoBehaviour
 {
-    public GameObject itemPrefab;
+    public GameObject[] objectsToSpawn; // Array of prefabs
+    public int minAmount = 2;
+    public int maxAmount = 7;
     public int Health = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,7 +20,7 @@ public class MinableScript : MonoBehaviour
 	    if (Health <= 0)
         {
             Destroy(gameObject); 
-            Instantiate(itemPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            SpawnDrops(UnityEngine.Random.Range(minAmount, maxAmount + 1));
         }
 	
     }
@@ -28,5 +30,20 @@ public class MinableScript : MonoBehaviour
     {
         Debug.Log(gameObject.name + " was clicked!");
         Health -= 10;
+    }
+
+    
+
+    void SpawnDrops(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 MoveAmount = new Vector3 (UnityEngine.Random.Range(-1.5f, 1.5f), 0f,0f);
+            Vector3 SlightMove = MoveAmount + gameObject.transform.position;
+            // Random object from array
+            GameObject prefab = objectsToSpawn[UnityEngine.Random.Range(0, objectsToSpawn.Length)];
+            Instantiate(prefab, SlightMove, gameObject.transform.rotation);
+            Debug.Log(amount);
+        }
     }
 }
